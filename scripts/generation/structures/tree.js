a85d50d51331c1a6d7faac78b26f4e0646f3130b
Copot Matei <matei@copot.eu>
2014-04-18 17:36:31 +0200

    trees adjustments
    Their leaves seem a bit better, and the size is now controllable

--- scripts/generation/structures/tree.js
+++ scripts/generation/structures/tree.js
@@ -4,26 +4,28 @@ define(['../../blocks', 'mathUtils'], function(Blocks, mathUtils){
         this.y=y;
         this.trunk=trunk;
         this.leaf=leaves;
-        this.size=3;
+        this.size=Math.floor(Math.random()*10);
         this.world=world;
     }
     Tree.prototype.addBranch=function(x, y){
         if(this.world.checkBlock(x, y, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10])){
-            ++this.size;
+            --this.size;
             this.world.cells[x][y] = new this.trunk.gen(x, y);
-            if(this.world.checkBlock(x+1, y, ['air'])) this.world.cells[x+1][y] = new this.leaf.gen(x+1, y);
-            if(this.world.checkBlock(x-1, y, ['air'])) this.world.cells[x-1][y] = new this.leaf.gen(x-1, y);
-            if(this.world.checkBlock(x, y+1, ['air'])) this.world.cells[x][y+1] = new this.leaf.gen(x, y+1);
-            if (mathUtils.getRandomBoolean(0.6)){
+            if(this.world.checkBlock(x+1, y, ['air'])) this.world.cells[x+1][y]=new this.leaf.gen(x+1, y);
+            if(this.world.checkBlock(x-1, y, ['air'])) this.world.cells[x-1][y]=new this.leaf.gen(x-1, y);
+            if(this.world.checkBlock(x, y+1, ['air'])) this.world.cells[x][y+1]=new this.leaf.gen(x, y+1);
+            if(this.world.checkBlock(x+1, y+1, ['air'])) this.world.cells[x+1][y+1]=new this.leaf.gen(x+1, y+1);
+            if(this.world.checkBlock(x-1, y+1, ['air'])) this.world.cells[x-1][y+1]=new this.leaf.gen(x-1, y+1);
+
+            if(this.world.checkBlock(x+2, y, ['air'])) this.world.cells[x+2][y]=new this.leaf.gen(x+2, y);
+            if(this.world.checkBlock(x-2, y, ['air'])) this.world.cells[x-2][y]=new this.leaf.gen(x-2, y);
+            if(this.world.checkBlock(x, y+2, ['air'])) this.world.cells[x][y+2]=new this.leaf.gen(x, y+2);
+            if(this.world.checkBlock(x+2, y+2, ['air'])) this.world.cells[x+2][y+2]=new this.leaf.gen(x+2, y+2);
+            if(this.world.checkBlock(x-2, y+2, ['air'])) this.world.cells[x-2][y+2]=new this.leaf.gen(x-2, y+2);
+            if (this.size>=0){
                 if (mathUtils.getRandomBoolean(0.4)) this.addBranch(x + 1, y + 1);
                 if (mathUtils.getRandomBoolean(0.4)) this.addBranch(x - 1, y + 1);
                 if (mathUtils.getRandomBoolean(0.6)) this.addBranch(x, y + 1);
-            } else {
-                if(this.world.checkBlock(x+1, y, ['air'])) this.world.cells[x+1][y]=new this.leaf.gen(x+1, y);
-                if(this.world.checkBlock(x-1, y, ['air'])) this.world.cells[x-1][y]=new this.leaf.gen(x-1, y);
-                if(this.world.checkBlock(x, y+1, ['air'])) this.world.cells[x][y+1]=new this.leaf.gen(x, y+1);
-                if(this.world.checkBlock(x+1, y+1, ['air'])) this.world.cells[x][y+1]=new this.leaf.gen(x+1, y+1);
-                if(this.world.checkBlock(x-1, y+1, ['air'])) this.world.cells[x][y+1]=new this.leaf.gen(x-1, y+1);
             }
         }
     };
