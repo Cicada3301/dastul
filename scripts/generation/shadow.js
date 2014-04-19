8fcf2b553c74464bba74016aa7c87fd7ac747c8c
Copot Matei <matei@copot.eu>
2014-04-19 17:59:20 +0200

    better shadows
    shadows can go sideways aswell now

--- scripts/generation/shadow.js
+++ scripts/generation/shadow.js
@@ -10,6 +10,51 @@ define(['blocks'], function(Blocks){
                     }
                 }
             }
+            function cicle() {
+                for (var row = 0; row < world.height; ++row) {
+                    var light = 0;
+                    for (var col = 0; col < world.width; ++col) {
+                        if (world.checkBlock(col, row, Blocks.ids)) {
+                            var current = world.cells[col][row];
+                            if (current.lightLevel > light) {
+                                light = current.lightLevel
+                            } else {
+                                light -= (1 - Blocks[Blocks.ids[world.cells[col][row].id]].transparency);
+                                current.lightLevel = light;
+                            }
+                        }
+                    }
+                }
+                for (var row = 0; row < world.height; ++row) {
+                    var light = 0;
+                    for (var col = world.width - 1; col >= 0; --col) {
+                        if (world.checkBlock(col, row, Blocks.ids)) {
+                            var current = world.cells[col][row];
+                            if (current.lightLevel > light) {
+                                light = current.lightLevel
+                            } else {
+                                light -= (1 - Blocks[Blocks.ids[world.cells[col][row].id]].transparency);
+                                current.lightLevel = light;
+                            }
+                        }
+                    }
+                }
+                for (var col = 0; col < world.width; ++col) {
+                    var light = 0;
+                    for (var row = world.height; row >= 0; --row) {
+                        if (world.checkBlock(col, row, Blocks.ids)) {
+                            var current = world.cells[col][row];
+                            if (current.lightLevel > light) {
+                                light = current.lightLevel
+                            } else {
+                                light -= (1 - Blocks[Blocks.ids[world.cells[col][row].id]].transparency);
+                                current.lightLevel = light;
+                            }
+                        }
+                    }
+                }
+            }
+            for(var i=0; i<5; ++i) cicle();
         }
     };
     return shadow;
